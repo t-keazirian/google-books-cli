a980bebcd7386b9cd331124538017cabca549631
Taylor Keazirian <taylork26@gmail.com>
2021-06-28 15:51:18 -0500

    updated help command

--- commands/help.js
+++ commands/help.js
@@ -2,29 +2,33 @@ const chalk = require('chalk');
 
 const menus = {
 	main: chalk.red`
-    gbooks-cli [command] --flag <options>
+  gbooks-cli [command] --flag <options>
 
-    SEARCH: search --keyword yourKeywordHere ........ search for 5 books with given keyword 
-    - please use quotes for keyword with spaces
+  List of available commands:
 
-    SAVE: save --id yourIdHere ........ save selected book with ID to reading list
+  SEARCH: search --keyword yourKeywordHere ........ search for 5 books with given keyword 
+  - please use quotes for keyword with spaces
 
-    LIST: list ........ print your reading list
+  SAVE: save --id yourIdHere ........ save selected book with ID to reading list
 
-    HELP: help ........ show help menu for a command
+  LIST: list ........ print your reading list
+
+  HELP: help ........ show help menu for a command
+
+  * help for individual commands and examples, type 'gbooks-cli help' + either: search, save, or list *
   `,
 	search: chalk.green`
     gbooks-cli search --keyword <options>
     * please use quotes for keyword with spaces *
     Example: gbooks-cli search --keyword 'stephen king'
   `,
-	save: chalk.blue`
+	save: chalk.green`
   gbooks-cli save --id <idGoesHere>
   Example: gbooks-cli save --id pXISEAAAQBAJ
   `,
-  list: chalk.yellow`
+	list: chalk.green`
   gbooks-cli list
-  `
+  `,
 };
 
 module.exports = args => {
